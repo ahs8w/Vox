dee4496e8283f110a721bf09c3eb9b1c52d7d62c
Adam Schiller <ahs8w@virginia.edu>
2013-07-30 16:21:13 +0200

    revisited project anew and updated local changes (mostly deletions of profiles and coffeescript related items).  General housecleaning.  No contributions of value.

---
+++ app/assets/javascripts/rating_form.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  // submits form (saves data) after user makes a change.
+//   $('form.rating_form #rating_button').change(function() {
+//     $('form.rating_form').submit();
+//   });
+// });
